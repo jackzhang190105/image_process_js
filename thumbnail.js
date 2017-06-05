e964a06455beb3b9bf1b695ef658a31feb73189f
Jack_wei Zhang (张维) <SPREADTRUM\jack_wei.zhang@sh66179tmp458.spreadtrum.com>
2017-06-05 16:04:42 +0800

    support thumbnail nearest algorithm
    TODO: thumbnail bilinear inerpolation
    
    	modified:   process_img.html
    	modified:   thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,6 @@
-
+/*
+ *	https://www.cnblogs.com/bigpo/p/4126806.html
+ */
 // 隔行采样，丢失信息
 function algo_thumbnail_interlace_sample(canvas_img_data)  {
 	var zoom=4;
@@ -28,7 +30,7 @@ function algo_thumbnail_interlace_sample(canvas_img_data)  {
 }
 
 function xy_2_array_pos(x, y, w) {
-	return (x*w + y)*4;
+	return (x + y*w)*4;
 }
 
 function get_rgb(x, y, img_data) {
@@ -39,22 +41,61 @@ function get_rgb(x, y, img_data) {
 	rgb[2] = img_data.data[pos+2];
 	rgb[3] = img_data.data[pos+3];
 
+	if (x==1000) {
+		console.log(y+ " "+ pos + " " + img_data.data[pos] + " " + rgb);
+	}
+
 	return rgb;
 }
 
-var zoom = 2;
-function algo_thumbnail(canvas_img_data, move_x, move_y)  {
+function set_rgb(rgb, x, y, img_data) {
+	var pos = xy_2_array_pos(x, y, img_data.width);
+	img_data.data[pos] = rgb[0];
+	img_data.data[pos+1] = rgb[1];
+	img_data.data[pos+2] = rgb[2];
+	img_data.data[pos+3] = rgb[3];
+}
 
-	var dest_width = canvas_img_data.width + move_x;
-	var dest_heigth = canvas_img_data.height + move_y;
-	var ratio_w = canvas_img_data.width/dest_width;
-	var ratio_h = canvas_img_data.width/dest_heigth;
+function thumbnail_nearest(canvas_img_data, step_w, step_h, dest_w, dest_h) {
+	var new_img_data = g_my_canvas_cxt_algo.createImageData(dest_w, dest_h);
+	var sum_x = 0;
+	var sum_y = 0;
 
-	alert("haha " + move_x + " "+move_y + " " +dest_width + " " + dest_heigth + " " + ratio_w + " " + ratio_h);
 
+	for (let i=0; i<dest_w*dest_h*4;i++)
+		new_img_data.data[i] = 200;
+
+	for (let x=0; x<dest_w; x++) {
+		sum_x++;
+		sum_y = 0;
+		for (let y=0; y<dest_h; y++) {
+			var x_d = Math.round(x*step_w);
+			var y_d = Math.round(y*step_h);
+			var rgb = get_rgb(x_d,y_d, canvas_img_data);
+			set_rgb(rgb, x, y, new_img_data);
+			sum_y++;
+
+			if (x==1000) {
+				console.log(rgb);
+			}
+		}
+	}
+
+	console.log("nearest " + sum_x + " " + sum_y + " " + new_img_data.width + "," + dest_w);
 	return new_img_data;
 }
 
-function thumbnail_nearest(canvas_img_data, ratio_w, ratio_h) {
+function algo_thumbnail(canvas_img_data, ratio_w, ratio_h)  {
 
+	var dest_width = Math.round(canvas_img_data.width * ratio_w);
+	var dest_heigth = Math.round(canvas_img_data.height * ratio_h);
+	var step_w = canvas_img_data.width/dest_width;
+	var step_h = canvas_img_data.height/dest_heigth;
+
+	console.log(canvas_img_data.width + " " +  canvas_img_data.height+ " " +dest_width + " " + dest_heigth + " " + ratio_w+ " " + ratio_h);
+	console.log("haha " +  " " + " " +dest_width + " " + dest_heigth + " " + step_w + " " + step_h);
+
+	return thumbnail_nearest(canvas_img_data, step_w, step_h, dest_width, dest_heigth);
 }
+
+
